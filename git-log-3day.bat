8094b3202567c23dc4b93025e5f0768c2970e9f5
SLathrop <Steve@SteLa.io>
2018-09-17 11:06:48 -0400

    Add 3 day variant of log script

---
+++ git-log-3day.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set GLT=%DATE:/=.%
+set GLT=%GLT: =.%
+
+echo %GLT%
+git log --pretty=oneline --abbrev-commit --no-merges --since=3.day --reverse --format=%%s
+
+echo %GLT% > "%HOME%\Git.Log.%GLT%.txt"
+git log --pretty=oneline --abbrev-commit --no-merges --since=3.day --reverse --format=%%s >> "%HOME%\Git.Log.%GLT%.txt"
+
+echo Opening "%HOME%\Git.Log.%GLT%.txt"...
+
+cmd /C notepad "%HOME%\Git.Log.%GLT%.txt"
